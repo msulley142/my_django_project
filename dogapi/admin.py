788bb6af0f25b64aac6d88cea5a422327edc86ae
Michael Sullivan <michaelsullivan@Michaels-Mac-mini.local>
2025-10-06 14:24:51 -0500

    minor fixes

--- dogapi/admin.py
+++ dogapi/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Dog, Breed
+
+class DogAdmin(admin.ModelAdmin):
+   
+    list_display = ('name', 'age', 'breed')
+   
+    search_fields = ('name', 'breed')
+
+    
+    list_filter = ('age', 'breed')
+
+   
+    list_display_links = ('name',)
+
+   
+    fields = ('name', 'age', 'breed')
+
+
+
+
 
 # Register your models here.
+admin.site.register(Dog)
+admin.site.register(Breed)
