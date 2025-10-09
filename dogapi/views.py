f885a9a0678b2925a2dee6c1634153f4928062b0
Michael Sullivan <michaelsullivan@Michaels-Mac-mini.local>
2025-10-09 00:03:53 -0500

    View Version 1

--- dogapi/views.py
+++ dogapi/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from .models import Dog, Breed
 from .serializers import DogSerializer, BreedSerializer
 # Create your views here.
-
+# Version 1
 class DogViewSet(viewsets.ModelViewSet):
     queryset = Dog.objects.all().select_related('breed').all().order_by('name')
     serializer_class = DogSerializer
